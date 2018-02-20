09a93ab1923ae0124024164e0390963405da7110
Tonizer <tiantan@usc.edu>
2018-02-19 17:43:44 -0800

    Fixed BatWithWaves early presence of wave after recycling. Standardize code style for BrickControl and SoundWaveControl.

--- Assets/Flappy Bird Style/Scripts/BrickControl.cs
+++ Assets/Flappy Bird Style/Scripts/BrickControl.cs
@@ -8,66 +8,54 @@ public class BrickControl : MonoBehaviour {
     private Rigidbody2D rb2d;
     private Animator anim;
 
-    public float fallinterval = 0.8f;
+	public readonly float FALL_INTERVAL = 0.8f;
+	private readonly int MAX_INDEX_WAVE = 3;
+	private readonly int FRAME_X_LEFT = -12;
+	private readonly int FRAME_X_RIGHT = 8;
+	private readonly float GRAVITY_SCALE_BRICK = 0.5f;
+	private readonly float BRICK_LEFT_FORCE = -200f;
     private float timeElaspe = 0f;
     private bool shouldTrigger = true;
     private int curIndex = 0;
 
-    // Use this for initialization
-    void Start () 
-    {
+    void Start () {
         rb2d = GetComponent<Rigidbody2D>();
         anim = transform.Find("tower").gameObject.GetComponent<Animator>();
-        brick = new GameObject[3];
+		brick = new GameObject[MAX_INDEX_WAVE];
         brick[0] = transform.Find("b1").gameObject;
         brick[1] = transform.Find("b2").gameObject;
         brick[2] = transform.Find("b3").gameObject;
 
-        //Start the object moving.
         rb2d.velocity = new Vector2 (GameControl.instance.scrollSpeed, 0);
     }
 
-    void Update()
-    {
-        if (transform.position.x < 8 && transform.position.x > -12)
-        {
-            if (shouldTrigger)
-            {
+    void Update() {
+		if (transform.position.x < FRAME_X_RIGHT && transform.position.x > FRAME_X_LEFT) {
+            if (shouldTrigger) {
                 anim.SetTrigger("Shrink");
                 shouldTrigger = false;
-            }
-            timeElaspe += Time.deltaTime;
-            if (timeElaspe >= fallinterval && curIndex < 3)
-            {
+            } timeElaspe += Time.deltaTime;
+			if (timeElaspe >= FALL_INTERVAL && curIndex < MAX_INDEX_WAVE) {
                 brick[curIndex].SetActive(true);
                 brick[curIndex].GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
-                brick[curIndex].GetComponent<Rigidbody2D>().gravityScale = 0.5f;
-                brick[curIndex].GetComponent<Rigidbody2D>().AddForce(new Vector2(-200f,0f));
+				brick[curIndex].GetComponent<Rigidbody2D>().gravityScale = GRAVITY_SCALE_BRICK;
+				brick[curIndex].GetComponent<Rigidbody2D>().AddForce(new Vector2(BRICK_LEFT_FORCE,0f));
 
                 curIndex++;
                 timeElaspe = 0f;
             }
-        }
-        else if (transform.position.x < -12)
-        {
-            if (!shouldTrigger)
-            {
+		} else if (transform.position.x < FRAME_X_LEFT) {
+            if (!shouldTrigger) {
                 anim.SetTrigger("Still");
                 shouldTrigger = true;
                 curIndex = 0;
-                for (int i = 0; i < 3; i++) 
-                {
+				for (int i = 0; i < MAX_INDEX_WAVE; i++) {
                     brick[i].GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
                     brick[i].transform.position = new Vector2(rb2d.transform.position.x, 2f);
                     brick[i].SetActive(false);
                 }
             }
-        }
-            
-
-        // If the game is over, stop scrolling.
-        if (GameControl.instance.gameOver == true)
-        {
+        } if (GameControl.instance.gameOver == true) {
             rb2d.velocity = Vector2.zero;
         }
     }
--- Assets/Flappy Bird Style/Scripts/SoundWaveControl.cs
+++ Assets/Flappy Bird Style/Scripts/SoundWaveControl.cs
@@ -6,52 +6,52 @@ public class SoundWaveControl : MonoBehaviour {
 
 	private GameObject[] waves;
 	private Rigidbody2D rb2d;
-
-	public float shootInterval = 0.8f;
 	private float timeElaspe = 0f;
 	private int curIndex = 0;
+	private readonly int FRAME_X = 12;
+	private readonly float WAVE_TO_BAT_X = 1.7f;
+	private readonly int MAX_INDEX_WAVE = 2;
+	private readonly float WAVE_LEFT_FORCE = -200f;
 
-	// Use this for initialization
+	public float shootInterval = 0.8f;
+
 	void Start () {
-		rb2d = GetComponent<Rigidbody2D>();
-		waves = new GameObject[2];
+		rb2d = GetComponent<Rigidbody2D>(); // BatWithWave's Rigidbody2D.
+		waves = new GameObject[MAX_INDEX_WAVE];
 		waves[0] = transform.Find("Soundwave1").gameObject;
 		waves[1] = transform.Find("Soundwave2").gameObject;
 
-		rb2d.velocity = new Vector2 (GameControl.instance.scrollSpeed, 0);
+		rb2d.velocity = new Vector2 (GameControl.instance.scrollSpeed, 0); // Make speed of BatWithWave to be the same as background.
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-		if (transform.position.x < 12 && transform.position.x > -12) {
+		if (transform.position.x < FRAME_X && transform.position.x > -FRAME_X) {
+			// When in the frame scene.
 			timeElaspe += Time.deltaTime;
-			if (timeElaspe >= shootInterval && curIndex < 3) {
+			if (timeElaspe >= shootInterval && curIndex < MAX_INDEX_WAVE + 1) {
 				waves [curIndex].SetActive (true);
 				waves [curIndex].GetComponent<Rigidbody2D> ().bodyType = RigidbodyType2D.Dynamic;
-				waves [curIndex].GetComponent<Rigidbody2D> ().AddForce (new Vector2 (-200f, 0));
-				waves [curIndex].GetComponent<Rigidbody2D> ().gravityScale = 0f;
+				waves [curIndex].GetComponent<Rigidbody2D> ().AddForce (new Vector2 (WAVE_LEFT_FORCE, 0)); // Make speed of wave faster than background. And left-ward.
+				waves [curIndex].GetComponent<Rigidbody2D> ().gravityScale = 0f; // No gravity.
 				curIndex++;
-				if (curIndex == 2) {
+				if (curIndex == MAX_INDEX_WAVE) {
 					curIndex = 0;
 				}
 				timeElaspe = 0f;
 			}
 		} else if (transform.position.x < -12) {
-
-				for (int i = 0; i < 2; i++) {
-					waves[i].GetComponent<Rigidbody2D> ().bodyType = RigidbodyType2D.Kinematic;
-					waves[i].transform.position = new Vector2 (rb2d.transform.position.x - 1.7f, rb2d.transform.position.y);
-
-			}
-		}
-
-		// If the game is over, stop scrolling.
-		if (GameControl.instance.gameOver == true)
-		{
-			rb2d.velocity = Vector2.zero;
-			for (int i = 0; i < 2; i++) {
+			// When move out of the scene.
+			for (int i = 0; i < MAX_INDEX_WAVE; i++) {
 				waves[i].GetComponent<Rigidbody2D> ().bodyType = RigidbodyType2D.Kinematic;
-				waves[i].transform.position = new Vector2 (rb2d.transform.position.x - 1.7f, rb2d.transform.position.y);
+				waves [curIndex].SetActive (false);
+				waves[i].transform.position = new Vector2 (rb2d.transform.position.x - WAVE_TO_BAT_X, rb2d.transform.position.y);
+			}
+		} if (GameControl.instance.gameOver == true) {
+			// When game is over.
+			rb2d.velocity = Vector2.zero;
+			for (int i = 0; i < MAX_INDEX_WAVE; i++) {
+				waves[i].GetComponent<Rigidbody2D> ().bodyType = RigidbodyType2D.Kinematic;
+				waves[i].transform.position = new Vector2 (rb2d.transform.position.x - WAVE_TO_BAT_X, rb2d.transform.position.y);
 				waves[i].SetActive(false);
 			}
 		}
